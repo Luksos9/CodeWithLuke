d9fde351340ba2e9dc8aa90b7785f2862c78e8d9
Luksos9 <lukaszszumilas9.com>
2023-12-23 13:31:23 +0100

    added .env and expanded authROutes, now i implement the mongoDB connection

--- CodeWithLuke/app.js
+++ CodeWithLuke/app.js
@@ -1,19 +1,38 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+// Require route modules
 const coursesRoutes = require('./coursesRoutes');
 const forumRoutes = require('./forumRoutes');
 const challengeRoutes = require('./challengeRoutes');
 const authRoutes = require('./authRoutes');
 
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/CodeWithLuke', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Define routes
 app.get('/', (req, res) => {
   res.send('Welcome to CodeWithLuke!');
 });
 
 app.use('/courses', coursesRoutes);
 app.use('/forum', forumRoutes);
-app.use('/challenges', challengeRoutes); // Add this line to use challengeRoutes
-app.use('/auth', authRoutes); // Add this line to use authRoutes
+app.use('/challenges', challengeRoutes);
+app.use('/auth', authRoutes);
 
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- CodeWithLuke/authRoutes.js
+++ CodeWithLuke/authRoutes.js
@@ -1,18 +1,38 @@
-const express = require('express');
-const router = express.Router();
-
-// User login route
-router.post('/login', (req, res) => {
-  // Authentication logic will go here
-  res.send('Login logic will be implemented here.');
-});
-
-// User registration route
-router.post('/register', (req, res) => {
-  // Registration logic will go here
-  res.send('Registration logic will be implemented here.');
-});
-
-// More routes can be added here for password reset, profile updates, etc.
-
-module.exports = router;
+router.post('/register', async (req, res) => {
+    try {
+      // Validation logic here
+      const { username, password } = req.body;
+      if (!username || !password) {
+        return res.status(400).send('Please enter all fields');
+      }
+  
+      // Check for existing user
+      let user = await User.findOne({ username });
+      if (user) {
+        return res.status(400).send('User already exists');
+      }
+  
+      user = new User({
+        username,
+        password
+      });
+  
+      // Save user with hashed password
+      await user.save();
+  
+      // Create token
+      const payload = { user: { id: user.id } };
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        { expiresIn: '5h' },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      res.status(500).send('Server error');
+    }
+  });
+  
