9114ded41091b2d523e71c5d66f592c60340256c
Luksos9 <lukaszszumilas9.com>
2023-12-23 12:34:10 +0100

    added app.js and basic server run

--- CodeWithLuke/app.js
+++ CodeWithLuke/app.js
@@ -1,1 +1,11 @@
-console.log('hehe')
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to CodeWithLuke!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
